6398e16f253ec3f1a7c5008ac85704a6065582de
bizu <scbizu@icloud.com>
2016-04-05 20:55:02 +0800

    新增准确度算子
    新增脸型过程中的准确度算子

--- face/processor.class.php
+++ face/processor.class.php
@@ -25,6 +25,11 @@ class processor{
 	private $LineArray=array(
 		
 	);
+	//准确度算子
+	private $firstStep;
+		
+	private $thirdStep;
+		
 	//准确度  也即过程分?
 	public static $CLprocessRate=0;
 	public static $GZLprocessRate=0;
@@ -58,6 +63,9 @@ class processor{
 			'tl'=>$this->Mandibular	
 		);
 		$this->fixarray=$analyse->chinK_array();
+		
+		
+		
 	}
 /**
  * 比较三线长度
@@ -67,6 +75,14 @@ class processor{
 		$twoline=($this->LineArray['fl']>$this->LineArray['sl'])?'fl':'sl';
 		$v_twoline=($this->LineArray['fl']>$this->LineArray['sl'])?$this->LineArray['fl']:$this->LineArray['sl'];
 		$finalline=($v_twoline>$this->LineArray['tl'])?$twoline:'tl';
+		$res=array();
+		array_push($res, $this->LineArray['fl']);
+		array_push($res, $this->LineArray['sl']);
+		array_push($res, $this->LineArray['tl']);
+		sort($res);
+		//这是一个神奇的算子  嘛  就把他叫做魔术因子吧
+		$this->firstStep=(($res[2]-$res[1])/$res[1]+($res[2]-$res[0])/$res[0])/2;
+		
 		return $finalline;
 	}
 	
@@ -80,45 +96,45 @@ class processor{
 		
 		switch ($long){
 			case 'fl':
-				self::$CLprocessRate+=0.33;
-				self::$FLprocessRate+=0.33;
-				self::$GZLprocessRate+=0.33;
+				self::$CLprocessRate+=$this->firstStep;
+				self::$FLprocessRate+=$this->firstStep;
+				self::$GZLprocessRate+=$this->firstStep;
 				if($this->width_check <= $this->height_check){
-					self::$CLprocessRate+=0.33;
+					self::$CLprocessRate+=($this->height_check-$this->width_check)/$this->width_check;
 					
 					$status='fl_h';
 				}else{
-					self::$FLprocessRate+=0.33;
-					self::$GZLprocessRate+=0.33;
+					self::$FLprocessRate+=$this->firstStep;
+					self::$GZLprocessRate+=$this->firstStep;
 					$status='fl_w';
 				}
 				break;
 			case 'sl':
-				self::$CLprocessRate+=0.33;
-				self::$EDprocessRate+=0.33;
-				self::$YLprocessRate+=0.33;
-				self::$FLprocessRate+=0.33;
+				self::$CLprocessRate+=$this->firstStep;
+				self::$EDprocessRate+=$this->firstStep;
+				self::$YLprocessRate+=$this->firstStep;
+				self::$FLprocessRate+=$this->firstStep;
 				if($this->width_check <= $this->height_check){
-					self::$CLprocessRate+=0.33;
+					self::$CLprocessRate+=($this->height_check-$this->width_check)/$this->width_check;
 					$status='sl_h';
 				}else{
-					self::$EDprocessRate+=0.33;
-					self::$YLprocessRate+=0.33;
-					self::$FLprocessRate+=0.33;
+					self::$EDprocessRate+=$this->firstStep;
+					self::$YLprocessRate+=$this->firstStep;
+					self::$FLprocessRate+=$this->firstStep;
 					$status='sl_w';
 				}	
 				break;
 			case 'tl':
-				self::$CLprocessRate+=0.33;
-				self::$CLprocessRate+=0.33;
-				self::$FLprocessRate+=0.33;
+				self::$CLprocessRate+=$this->firstStep;
+				self::$CLprocessRate+=$this->firstStep;
+				self::$FLprocessRate+=$this->firstStep;
 				if($this->width_check<=$this->height_check){
-					self::$CLprocessRate+=0.33;
+					self::$CLprocessRate+=($this->height_check-$this->width_check)/$this->width_check;
 					$status='tl_h';
 					
 				}else{
-					self::$CLprocessRate+=0.33;
-					self::$FLprocessRate+=0.33;
+					self::$CLprocessRate+=$this->firstStep;
+					self::$FLprocessRate+=$this->firstStep;
 					$status='tl_w';
 				}
 				break;
@@ -133,9 +149,14 @@ class processor{
  */	
 	public function lineFix(){
 		$tmp=$this->fixarray;
+		$this->thirdStep=abs($tmp[1]-$tmp[0])-abs($tmp[2]-$tmp[1]);
 		//判断斜率大小 
 		//依据为:直线方程求导所得为k=0的水平直线 而曲线方程求导所得的K必定大于零
 		if(abs($tmp[1]-$tmp[0])-abs($tmp[2]-$tmp[1])<$this->deviation){
+			
+			//又是一个算误差的神奇的算子
+			$this->thirdStep=(1/($this->thirdStep+$tmp[3]))/floatval(10);
+			
 			if($tmp[3]<$this->deviation){
 				return 'ao_chin';
 			}else{
@@ -153,7 +174,7 @@ class processor{
 		$status=$this->FacewidthVSheight();
 		switch ($status){
 			case 'fl_h':
-				//强行为了脸型匹配度
+
 				self::$CLprocessRate+=0.33;
 				$type='CL';
 				break;
@@ -161,10 +182,10 @@ class processor{
 				$fix=$this->lineFix();
 				if($fix==='l_chin'){
 					$type='GZL';
-					self::$GZLprocessRate+=0.33;
+					self::$GZLprocessRate+=$this->thirdStep;
 				}else{
 					$type='FL';
-					self::$FLprocessRate+=0.33;
+					self::$FLprocessRate+=$this->thirdStep;
 				}				
 				break;
 			case 'sl_h':
@@ -174,13 +195,13 @@ class processor{
 			case 'sl_w':
 				$fix=$this->lineFix();
 				if($fix==='ao_chin'){
-					self::$FLprocessRate+=0.33;
+					self::$FLprocessRate+=$this->thirdStep;
 					$type='FL';
 				}else if($fix==='l_chin'){
-					self::$EDprocessRate+=0.33;
+					self::$EDprocessRate+=$this->thirdStep;
 					$type='ED';
 				}else if($fix==='c_chin'){
-					self::$YLprocessRate+=0.33;
+					self::$YLprocessRate+=$this->thirdStep;
 					$type='YL';
 				}
 				break;
@@ -191,10 +212,10 @@ class processor{
 			case 'tl_w':
 				$fix=$this->lineFix();
 				if($fix==='l_chin'){
-					self::$CLprocessRate+=0.33;
+					self::$CLprocessRate+=$this->thirdStep;
 					$type='CL';
 				}else{
-					self::$FLprocessRate+=0.33;
+					self::$FLprocessRate+=$this->thirdStep;
 					$type='FL';
 				}
 			default:
